62103b9c96294849e1cbb83ab78dd26407b1da97
Luis Arce <luis.arce@jalasoft.com>
2015-10-08 22:18:31 -0400

    Generated the project using yeoman
    Signed-off-by: Luis Arce <luis.arce@jalasoft.com>

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name game1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the game1App
+ */
+angular.module('game1App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
